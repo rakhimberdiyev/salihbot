fb2aaad90f14b406f1e065dd33d1e17caf6b017b
rakhimberdiyev <163638265+rakhimberdiyev@users.noreply.github.com>
2024-03-17 00:11:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ async def on_startup(dispatcher):
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(on_startup())
