11d53d8869ffa7be6fc3125bc1cc49cfbbc87fd5
rakhimberdiyev <163638265+rakhimberdiyev@users.noreply.github.com>
2024-03-17 00:44:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,25 @@ import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
+import aiohttp
+import asyncio
 
+async def get_bot_info(bot_token):
+    async with aiohttp.ClientSession() as session:
+        url = f'https://api.telegram.org/bot{7152911619:AAFXeywJCs_xP1G081crl65q56Vtn5FowBk}/getMe'
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.json()
+                return data
+            else:
+                print("Telegram API'dan ma'lumot olishda xato yuz berdi")
+                return None
 
 async def on_startup(dispatcher):
-    
+    bot_token = '7152911619:AAFXeywJCs_xP1G081crl65q56Vtn5FowBk'  # Bot tokeningizni bu yerga kiriting
+    bot_info = await get_bot_info(bot_token)
+    if bot_info:
+        print(bot_info)
     try:
         db.create_table_users()
     except Exception as err:
